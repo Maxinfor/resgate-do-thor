42a84edfd826fc2fc36dd9a8a54ca88ea53d0672
Max <130712338+Maxinfor@users.noreply.github.com>
2026-06-26 01:27:57 -0300

    Refactor script.js to remove redundant code

--- script.js
+++ script.js
@@ -10,14 +10,12 @@ function evento(tipo) {
     let thor = document.getElementById("thor-img");
     let balao = document.getElementById("balao");
     
-    // Animação do Thor
     thor.style.left = "70%";
     setTimeout(() => { thor.style.left = "0%"; }, 2000);
 
-    // Lógica de Energia
     energia -= 10;
     document.getElementById("valor-energia").innerText = energia;
     document.getElementById("energia-bar").style.width = energia + "%";
     
-    // Atualiza fala
     balao.innerText = tipo === 'chuva' ? "Helô: Protegidas da chuva!" : "Liz: Óculos salvos da poeira!";
+}
