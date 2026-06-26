5fd8c3a2a12382ac1c8357089c8f496a22c568ee
Max <130712338+Maxinfor@users.noreply.github.com>
2026-06-26 15:06:37 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+﻿self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('thor-store').then((cache) => cache.addAll([
+    'index.html', 'style.css', 'script.js', 'Capa.jpg', 'Helo.jpg', 'Liz.jpg', 'Thor.jpg', 'latido thor.mp3'
+  ])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
