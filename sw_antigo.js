67bbee3bb1a1e3f7bc76082f63c74de1b626aa5b
Max <130712338+Maxinfor@users.noreply.github.com>
2026-06-27 02:03:54 -0300

    Delete sw_antigo.js

--- sw_antigo.js
+++
@@ -1,9 +0,0 @@
-﻿self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('thor-store').then((cache) => cache.addAll([
-    'index.html', 'style.css', 'script.js', 'capa.jpg', 'helo.jpg', 'liz.jpg', 'thor.jpg', 'latido thor.mp3'
-  ])));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
